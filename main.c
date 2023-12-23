6c9ea7383bf00f7c26a930259cb9965bbfd6af63
Myles Moylan <moylanm@fastmail.com>
2023-12-22 17:34:11 -0800

    Instruction execution

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char *argv[]) {
+	initVM();
+
 	Chunk chunk;
 	initChunk(&chunk);
 
 	int constant = addConstant(&chunk, 1.2);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
-
 	writeChunk(&chunk, OP_RETURN, 123);
 
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+
+	freeVM();
 	freeChunk(&chunk);
 
 	return 0;
