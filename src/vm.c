7198a844610586002ca9b8f6e71512b47f3bf368
Myles Moylan <moylanm@fastmail.com>
2023-12-30 13:06:05 -0800

    Statements

--- src/vm.c
+++ src/vm.c
@@ -106,6 +106,7 @@ static InterpretResult run() {
 			case OP_NIL: push(NIL_VAL); break;
 			case OP_TRUE: push(BOOL_VAL(true)); break;
 			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_POP: pop(); break;
 			case OP_EQUAL: {
 				Value b = pop();
 				Value a = pop();
@@ -140,9 +141,12 @@ static InterpretResult run() {
 				}
 				push(NUMBER_VAL(-AS_NUMBER(pop())));
 				break;
-			case OP_RETURN: {
+			case OP_PRINT: {
 				printValue(pop());
 				printf("\n");
+				break;
+			}
+			case OP_RETURN: {
 				return INTERPRET_OK;
 			}
 		}
